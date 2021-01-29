aa8a0b844fe19fd8d6f737467b076dbe6ce3526a
Andre Salzmann <salzi_andre@web.de>
2021-01-29 12:04:29 +0100

    Replaced Getter und Setter with Lombok Annotation

--- src/main/java/de/format/salzzy/Rechnungsmanager/model/User.java
+++ src/main/java/de/format/salzzy/Rechnungsmanager/model/User.java
@@ -3,22 +3,17 @@ package de.format.salzzy.Rechnungsmanager.model;
 import java.util.Collection;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name="users")
+@Getter
+@Setter
 public class User{
 	
 	@Id
@@ -35,63 +30,13 @@ public class User{
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="infoId", referencedColumnName="user_id", insertable=true, updatable=true)
 	private UserInfo userinfo;
+
+	@OneToMany(mappedBy = "owner")
+	private Set<Document> document;
 	
 	@ManyToMany
 	private Set<Role> roles;
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Set<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
-	
-	public String getPasswordConfirm() {
-        return passwordConfirm;
-    }
-
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
-    }
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public UserInfo getUserinfo() {
-		return userinfo;
-	}
-
-	public void setUserinfo(UserInfo userinfo) {
-		this.userinfo = userinfo;
-	}
-
-
-	
-	
-    
-	
 	
 
 }
