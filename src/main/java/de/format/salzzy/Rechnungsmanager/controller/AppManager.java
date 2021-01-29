005bb17cba7c68a976ae40aedf35af1d365acd37
Andre Salzmann <salzi_andre@web.de>
2021-01-29 12:09:10 +0100

    preparing dashboard for documentService usage

--- src/main/java/de/format/salzzy/Rechnungsmanager/controller/AppManager.java
+++ src/main/java/de/format/salzzy/Rechnungsmanager/controller/AppManager.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import de.format.salzzy.Rechnungsmanager.model.Document;
+import de.format.salzzy.Rechnungsmanager.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -37,35 +39,30 @@ public class AppManager {
 	 * Test Ordner in dem die PDF liegt
 	 * und alles andere passiert
 	 */
-	private static String INPUT_DIR = "C:\\Users\\salzmann\\Desktop\\Test-Umgebung\\FIBU\\Mitarbeiter\\{placeholder}";
-	private static String BACKUP_DIR = "C:\\Users\\salzmann\\Desktop\\Test-Umgebung\\FIBU\\Mitarbeiter\\{placeholder}\\Backup\\";
 	private static String FERTIG_DIR = "C:\\Users\\salzmann\\Desktop\\Test-Umgebung\\FIBU\\Rechnungen_Freigegeben\\";
-	
-	@Autowired
+
 	private UserService userService;
+	private DocumentService documentService;
+
+	@Autowired
+	public AppManager(UserService userService, DocumentService documentService)
+	{
+		this.documentService = documentService;
+		this.userService = userService;
+	}
 
 	@GetMapping("/dashboard")
 	public String home(Model theModel, @RequestParam("name") Optional<String> name)  {
 		
-		User user = getCurrentUser();
-		String userFolder = INPUT_DIR.replace("{placeholder}", user.getUsername());
-		
-		INPUT_DIR = INPUT_DIR.replace("{placeholder}", user.getUsername());
-		BACKUP_DIR = BACKUP_DIR.replace("{placeholder}", user.getUsername());
-		
-		String userBackupFolder = BACKUP_DIR.replace("{placeholder}", user.getUsername());
+		User user = userService.currentLoggedInUser();
+		String userFolder = documentService.getUserDocumentPath(user);
 		
 		// Check ob das Pdf Verzeichniss existiert
 		File PDF_DIR = new File(userFolder);
-		File BACKUP_DIR = new File(userBackupFolder);
 		
 		// Erstelle Pdf Verzeichniss
 		if(!PDF_DIR.exists()) {
 			PDF_DIR.mkdir();
-		}
-		
-		if(!BACKUP_DIR.exists()) {
-			BACKUP_DIR.mkdir();
 		}
 		
 		// Get all pdfs im Verzeichniss des Users
@@ -81,15 +78,10 @@ public class AppManager {
 				}
 			}
 		} 
-		
-		
-		// Übergebe Liste mit Namen an Frontend
-		theModel.addAttribute("pdfs", fileNames);
 
-		// Anzahl an Rechnungen im Ordner
+		theModel.addAttribute("pdfs", fileNames);
 		theModel.addAttribute("anzahl", fileNames.size());
-		
-		// return startseite
+
 		return "app/dashboard/index";
 	}
 	
@@ -97,10 +89,8 @@ public class AppManager {
 	@PostMapping("/rechnungUpload")
 	public String uploadRechnung(@RequestParam("file") MultipartFile file, @RequestParam("dateiName") String dateiName) {
 		
-		User user = getCurrentUser();
-		
-		
-		String userFolder = INPUT_DIR.replace("{placeholder}", user.getUsername());
+		User user = userService.currentLoggedInUser();
+		String userFolder = documentService.getUserDocumentPath(user);
 		String destinationFilePath = userFolder + "\\" + file.getOriginalFilename();
 		
 		File fileDest = new File(destinationFilePath);
@@ -114,54 +104,34 @@ public class AppManager {
 	}
 	
 	@GetMapping("/delete")
-	public String uploadRechnung(@RequestParam("id") String fileName) {
+	public String deleteInvoice(@RequestParam("id") Integer documentId) {
 		
-		User user = getCurrentUser();
-		
-		File file = new File(INPUT_DIR + "\\" + user.getUsername() + "\\" + fileName);
-		file.delete();
+		User user = userService.currentLoggedInUser();
+		// delete document by ID
 		
 		return "redirect:/dashboard";
 	}
 	
 	
 	@GetMapping("/stempeln")
-	public String stempeln(@RequestParam(name="prs", required=false) boolean preislich, @RequestParam(name="sach", required=false) boolean sachlich, 
-							@RequestParam("ksst") String kostenstelle, @RequestParam("name") String fileName) {
-		
-		User user = getCurrentUser();
+	public String stempeln(@RequestParam(name="prs", required=false) boolean preislich,
+						   @RequestParam(name="sach", required=false) boolean sachlich,
+						   @RequestParam("ksst") String kostenstelle,
+						   @RequestParam("name") String fileName)
+	{
+		User user = userService.currentLoggedInUser();
 		fileName = fileName.replaceAll("_", " ");
-		
-		// Ordner erstellen
-		String userFolder = getUserFolder(user) + "\\";
-		String userBackupFolder = BACKUP_DIR.replace("{placeholder}", user.getUsername());
-		
-		// SignaturBild als Blob aus der Datenbank erhalten
+
+		String userFolder = documentService.getUserDocumentPath(user);
 		byte[] signaturBytes = user.getUserinfo().getSignatur();
 
 		// Pdf Stempeln 
 		try {
 			PdfStempeln.Stempeln(userFolder, preislich, sachlich, kostenstelle, fileName, user.getUsername(), signaturBytes);
-			
 			File signedPdf = new File(FERTIG_DIR + fileName);
-			File backUpPdf = new File(userBackupFolder + fileName);
 
-			System.out.println("Verschieben");
-			
-			// Datei kopieren
-			try(FileInputStream fis = new FileInputStream(signedPdf)){
-				FileOutputStream fos = new FileOutputStream(backUpPdf);
-				byte[] buffer = new byte[1024];
-				int length;
-				while((length = fis.read(buffer)) > 0) {
-					fos.write(buffer, 0, length);
-				}
-				fos.close();
-			}
-			
 			// Datei in User Ordner löschen
 			new File(userFolder + fileName).delete();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (DocumentException e) {
@@ -174,87 +144,57 @@ public class AppManager {
 	
 	
 	@GetMapping("/pdf")
-	public ResponseEntity<byte[]> getPDF(@RequestParam("name") String fileName, @RequestParam(name="verteiler", required=false) boolean verteiler) throws IOException {
-		
-		User user = getCurrentUser();
-		
-		fileName = fileName.replaceAll("_", " ");
-		
-		String filePath = getUserFolder(user) + "\\" + fileName;
-		if(verteiler) {
-			filePath = "C:\\Users\\salzmann\\Desktop\\Test-Umgebung\\FIBU\\Rechnungen\\" + fileName;
-		}
+	public ResponseEntity<byte[]> getPDF(@RequestParam("name") String fileName, @RequestParam(name="verteiler", required=false) boolean verteiler) throws IOException
+	{
+		User user = userService.currentLoggedInUser();
+		String fileNameWithoutSpace = fileName.replaceAll("_", " ");
+		String filePath = documentService.getUserDocumentPath(user) + fileNameWithoutSpace;
+
+		if(verteiler) filePath = documentService.getPublicInvoiceDocumentPath() + fileNameWithoutSpace;
+
 		File quellFile = new File(filePath);
-		
 		byte[] encodedBase64 = Files.readAllBytes(quellFile.toPath());
-		
+		String filename = quellFile.getName();
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.parseMediaType("application/pdf"));
-		String filename = quellFile.getName();
-		
 		headers.add("content-disposition", "inline;filename="+filename);
 		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-		ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(encodedBase64, headers, HttpStatus.OK);
-
-		return response;
+		return new ResponseEntity<byte[]>(encodedBase64, headers, HttpStatus.OK);
 	}
 	
 	@GetMapping("/png")
-	public ResponseEntity<byte[]> getPNG(@RequestParam("name") String fileName) throws IOException {
-		
-		User user = getCurrentUser();
-		
-		String filePath = getUserFolder(user) + "\\utils\\" + fileName;
+	public ResponseEntity<byte[]> getPNG(@RequestParam("name") String fileName) throws IOException
+	{
+		User user = userService.currentLoggedInUser();
+		String filePath = documentService.getUserDocumentPath(user) + "\\utils\\" + fileName;
 		File quellFile = new File(filePath);
-		
 		byte[] encodedBase64 = Files.readAllBytes(quellFile.toPath());
-		
+		String filename = quellFile.getName();
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.parseMediaType("application/png"));
-		String filename = quellFile.getName();
-		
 		headers.add("content-disposition", "inline;filename="+filename);
-		
 		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-		ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(encodedBase64, headers, HttpStatus.OK);
-		
-		return response;
+		return new ResponseEntity<byte[]>(encodedBase64, headers, HttpStatus.OK);
 	}
 	
 	
 	@GetMapping("/jpg")
-	public ResponseEntity<byte[]> getJPG(@RequestParam("name") String fileName) throws IOException {
-		
-		User user = getCurrentUser();
-		
-		String filePath = getUserFolder(user) + "\\utils\\" + fileName;
+	public ResponseEntity<byte[]> getJPG(@RequestParam("name") String fileName) throws IOException
+	{
+		User user = userService.currentLoggedInUser();
+		String filePath = documentService.getUserDocumentPath(user) + "\\utils\\" + fileName;
 		File quellFile = new File(filePath);
-		
 		byte[] encodedBase64 = Files.readAllBytes(quellFile.toPath());
-		
+		String filename = quellFile.getName();
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.parseMediaType("application/jpg"));
-		String filename = quellFile.getName();
-		
 		headers.add("content-disposition", "inline;filename="+filename);
-		
 		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-		ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(encodedBase64, headers, HttpStatus.OK);
-		
-		return response;
+		return new ResponseEntity<byte[]>(encodedBase64, headers, HttpStatus.OK);
 	}
-	
-	
-	private User getCurrentUser() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String currentPrincipalName = authentication.getName();
-		User user = userService.findByUsername(currentPrincipalName);
-		return user;
-	}
-	
-	private String getUserFolder(User user) {
-		String userFolder = INPUT_DIR.replace("{placeholder}", user.getUsername());	
-		return userFolder;
-	}
+
 	
 }
