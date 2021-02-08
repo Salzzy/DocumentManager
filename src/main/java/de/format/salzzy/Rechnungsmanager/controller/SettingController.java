a2871fb770c1e3df5043e73bbd8cde642006f1da
Andre Salzmann <salzi_andre@web.de>
2021-02-08 11:52:09 +0100

    removed function

--- src/main/java/de/format/salzzy/Rechnungsmanager/controller/SettingController.java
+++ src/main/java/de/format/salzzy/Rechnungsmanager/controller/SettingController.java
@@ -1,14 +1,8 @@
 package de.format.salzzy.Rechnungsmanager.controller;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import de.format.salzzy.Rechnungsmanager.service.DocumentService;
-import de.format.salzzy.Rechnungsmanager.service.SettingService;
-import org.apache.pdfbox.io.IOUtils;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,9 +15,6 @@ import org.springframework.web.multipart.MultipartFile;
 import de.format.salzzy.Rechnungsmanager.model.auth.User;
 import de.format.salzzy.Rechnungsmanager.model.UserInfo;
 import de.format.salzzy.Rechnungsmanager.service.UserService;
-import java.awt.*;
-
-import javax.imageio.ImageIO;
 
 @Controller
 public class SettingController {
@@ -67,15 +58,7 @@ public class SettingController {
 		String fullFilePath = documentService.saveFile(signatureImage, documentService.getUserDocumentUtilsPath(user));
 		user.getUserInfo().setSignatureFileName(signatureImage.getOriginalFilename());
 		userService.save(user);
-		removeWhiteBackground(fullFilePath);
 		return "redirect:/settings";
 	}
-
-
-	private void removeWhiteBackground(String filePath)
-	{
-
-	}
-
 
 }
